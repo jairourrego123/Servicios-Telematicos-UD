cc8e774f39738811e4a8b77381460777b420f2a1
jairourrego123 <jairourrego123@gmail.com>
2022-05-20 21:06:34 -0500

    Ejercicio3

---
+++ Clase_3/Ejercicio/js.js
@@ -0,0 +1,76 @@
+
+const express = require("express");
+const app = express();
+const puerto = 5000;
+const multer = require('multer')
+
+
+const { urlencoded } = require("express");
+app.use(urlencoded({extended:false}))
+// motor de plantillas 
+app.set('view engine', 'ejs');
+app.set('views',__dirname+'/views')
+app.use(express.static(__dirname + "/public"))
+
+
+const storage = multer.diskStorage({
+    
+    destination: __dirname+'/views/imagenes',
+    filename: (req,file,cb)=>{
+        cb(null,file.originalname)
+    }
+})
+
+
+
+app.use(multer({
+    storage:storage,
+    dest: __dirname+'/views/imagenes'
+}).single('imagen'))
+
+
+app.use(express.static(__dirname+'/views'))
+
+
+var producto = []
+contador=-1
+// atencion de la solicitud
+app.get('/',(req,res)=>{
+    res.render("index",{producto:producto})
+    
+});
+
+app.post('/',(req,res)=>{
+
+    
+    contador++
+    producto[contador]=[
+                        req.body.nombre,
+                        req.body.descripcion,
+                        req.file.originalname]
+    
+    res.render("index", {
+
+                    producto: producto
+    } )
+    
+    console.log(producto)
+    console.log(req.file)
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// Oyente
+app.listen(puerto,()=>{
+    console.log("Ejecutando el servidor local")
+})
